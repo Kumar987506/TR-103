b74110074d99078494483a76a0a3a6d37adad98f
Kumar987506 <sandeepkumarlearn@gmail.com>
2025-12-31 17:00:38 +0530

    Create index.js
    Initial implementation of index.js with various variable declarations, console logs, and array manipulations.

---
+++ index.js
@@ -0,0 +1,74 @@
+let Name = "sandeep";
+let age = 19;
+
+let isStudent = true;
+let hobbies = ["reading", "singing", "coding"];
+console.log(Name);
+console.log(age);
+console.log(isStudent);
+console.log(hobbies);
+
+console.log("hobbiesis :" + hobbies[0]);// print first element of array.
+
+let a= 23;
+if (a<=10) {
+    console.log("less")
+    
+}else{
+    console.log("gretter");
+}
+
+const obj = {
+    neme: "raaj", 
+    age : 24,
+    city: {
+        pin: 121212,
+     }
+    }
+    console.log(obj);
+    console.log(obj.city);
+
+    //concet, num=   is finit , isint , pareInt pareFloat , maxsafe integer .
+   // toPrecisre
+
+   //concat , push ,pop, reverce, slice, short ,unshift , shift , at , flat() , keys() , lenth, 
+
+
+
+   let cars = ['BMW', 'Mercedes'];
+let newLength = cars.unshift('Audi', 'Toyota');
+
+console.log(cars);        // Output: ['Audi', 'Toyota', 'BMW', 'Mercedes']
+console.log(newLength);//4
+console.log(cars[1]);
+ 
+let cities = ['New York', 'London', 'Paris'];
+let removedCity = cities.shift();
+
+console.log(cities);  
+console.log(removedCity);
+
+
+
+const nestedArray = [1, [2, 3], [4, [5, 6]]];
+
+console.log(nestedArray.flat());    
+// console.log(nestedArray.flat(2));     
+// console.log(nestedArray.flat(Infinity)); 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  
