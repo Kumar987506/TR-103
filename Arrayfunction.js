1d337edb731fc0040a9c1566c4d6298c65583ed0
Kumar987506 <sandeepkumarlearn@gmail.com>
2025-12-31 17:22:15 +0530

    arrayfunction

---
+++ Arrayfunction.js
@@ -0,0 +1,18 @@
+// Arowfunction
+const sum = a => a * a
+console.log(sum(4));
+
+const add = (a, b) => a + b
+console.log(add(3, 5));
+
+
+const subtract = (a, b) => a - b
+console.log(subtract(10, 4));
+
+
+const multiply = (a, b) => a * b
+console.log(multiply(2, 6));
+
+
+const divide = (a, b) => a / b
+console.log(divide(20, 4));
